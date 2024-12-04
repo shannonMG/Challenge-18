e315b62f5f463d08cd07ce277714a672d51771b4
ShannonMG <shannonmastrogiovanni@gmail.com>
2024-12-04 17:02:06 -0500

    removed .js

--- server/src/models/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const User_js_1 = __importDefault(require("./User.js"));
-const Book_js_1 = __importDefault(require("./Book.js"));
-exports.default = { User: User_js_1.default, Book: Book_js_1.default };
